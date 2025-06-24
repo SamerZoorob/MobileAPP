4c928bab7b92f78fa12863b1f39fea5e1291e89c
Samer <55001780+SamerZoorob@users.noreply.github.com>
2025-06-24 19:16:27 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/BusProject/User.java
@@ -0,0 +1,4 @@
+package com.example.BusProject;
+
+public class User {
+}
---
+++ app/src/main/java/com/example/BusProject/viewbuses.java
@@ -0,0 +1,6 @@
+package com.example.BusProject;
+
+import android.app.Activity;
+
+public class viewbuses extends Activity {
+}
